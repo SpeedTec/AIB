46de671f3715ad8fed3f83d57d46f7cc26ad3f9b
RogueX957 <57369170+RogueX957@users.noreply.github.com>
2022-09-04 13:28:34 -0300

    Add files via upload
    Archive with analogRead() module to understand the voltage values (changes will be made after physical tests).
    
    off course, we're not using real animals if you wonder.

---
+++ voltage-value.ino
@@ -0,0 +1,46 @@
+// We were having problems to create these variables inside the setup
+int sensorValue;
+float voltage;
+float lastVoltage;
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+}
+
+void vl(){
+  sensorValue = analogRead(A0);
+  voltage = sensorValue * (5.00 / 1023);
+}
+
+void loop(){
+  //giraffe = (voltage >= 0 and voltage <= 1.65);
+  //cow = (voltage >= 1.66 and voltage <= 3.34);
+  //elephant = (voltage >= 3.35 and voltage <= 5);
+
+  // Analog input is being made with a potentiometer for now.
+  // The idea is to use a weight tracking scale that works in the same way.
+  v();
+
+  if (voltage >= 0.55 and voltage <= 1.65){
+    Serial.println("Giraffe");
+    while(voltage > 0.55 and voltage <= 1.65){
+      vl();
+    }
+  }
+  else if(voltage >= 1.66 and voltage <= 3.34){
+    Serial.println("Cow");
+    lastVoltage = voltage;
+    while(voltage >= 1.66 and voltage <= 3.34){
+       vl();
+    }
+  }
+  else if(voltage >= 3.35 and voltage <= 5){
+    Serial.println("Elephant");
+    lastVoltage = voltage;
+    while(voltage >= 3.35 and voltage <= 5){
+      vl();
+    }
+  }
+  else {}
+}
