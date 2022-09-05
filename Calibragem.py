bfba6866aa9ceff3c04873f4de3535b5960bafa9
RogueX957 <57369170+RogueX957@users.noreply.github.com>
2022-09-05 15:52:23 -0300

    Update com a lógica funcionando

--- Calibragem.py
+++ Calibragem.py
@@ -3,16 +3,15 @@ voltage = float(input("Digite a tensão: "))
 lastVoltage = voltage
 
 while t1 < 3:
+
     if voltage >= (lastVoltage - (lastVoltage / 10)) and voltage <= (lastVoltage + (lastVoltage / 10)):
         t1 += 1
-        print("Passou")
+        print("Passou", t1)
+        lastVoltage = voltage
     else:
         t1 = 0
-        print("Não passou")
-        lastVoltage = 0
-        voltage = 0
+        print("Não passou", t1)
 
     voltage = float(input("Digite a tensão: "))
-    lastVoltage = voltage
 
 print("Calibragem concluída")
