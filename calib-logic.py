8a84776313bc973806e33c3054516cd09ad8f511
Gabriel Souza de Carvalho Lima <gabrielsclima.2004@gmail.com>
2022-09-08 01:52:47 -0300

    Strings translated to english

--- calib-logic.py
+++ calib-logic.py
@@ -5,16 +5,16 @@ voltage = float()
 lastVoltage = voltage
 
 while t1 < 3:
-    voltage = float(input("Digite a tensão: "))
+    voltage = float(input("Actual voltage "))
 
     if voltage >= (lastVoltage - (lastVoltage / 10)) and voltage <= (lastVoltage + (lastVoltage / 10)):
         t1 += 1
-        print("Passou", t1)
+        print("Pass", t1)
 
     else:
         t1 = 0
-        print("Não passou", t1)
+        print("Reset", t1)
 
     lastVoltage = voltage
 
-print("Calibragem concluída")
+print("Calibration concluded")
