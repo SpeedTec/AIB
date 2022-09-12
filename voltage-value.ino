72f1f6f453f6a445921b1467066a3f39f04d7c83
gabrielsclima <gabrielsclima.2004@gmail.com>
2022-09-12 19:36:27 -0300

    calib-logic.py converted to be interpreted on voltage-value.ino
    Now it can calibrate the values before showing the animal's name

--- voltage-value.ino
+++ voltage-value.ino
@@ -1,46 +1,58 @@
-int sensorValue;
+int t1;
 float voltage;
+float lastVoltage;
 
 void setup(){
   Serial.begin(9600);
   pinMode(A0, INPUT);
 }
 
-void updateVoltage(){
-  sensorValue = analogRead(A0);
-  voltage = sensorValue * (5.00 / 1023);
-}
-
 void loop(){
-  //giraffe = (voltage >= 0.55 and voltage <= 1.65);
-  //cow = (voltage >= 1.66 and voltage <= 3.34);
-  //elephant = (voltage >= 3.35 and voltage <= 5);
+  Serial.println("Etapa 0");
+  t1 = 0;
+  
+  while(voltage <= 0.54 or t1 < 24){
+      voltage = analogRead(A0) * (5.00 / 1023);
 
-  updateVoltage();
-
-  while(voltage <= 0.54){
-      Serial.println("Waiting object...");
-      updateVoltage();
-      delay(200);
+      if (voltage <= 0.54){
+        t1 = 0;
+        }
+      
+      else if (voltage >= (lastVoltage - (lastVoltage / 10)) and voltage <= (lastVoltage + lastVoltage / 10)){
+        t1 += 1;
+        }
+      
+      else{
+        t1 = 0;
+        }
+      delay(100);
+      lastVoltage = voltage;
   }
-  if (voltage <= 1.65){
+   Serial.println("Done!");
+  
+  if (voltage >= 0.55 and voltage <= 1.65){
     Serial.println("Giraffe");
-    while(voltage >= 0.55 and voltage <= 1.65){
-      updateVoltage();
+    
+    while(voltage >= 0.5){
+      voltage = analogRead(A0) * (5.00 / 1023);
       delay(200);
     }
   }
-  else if(voltage <= 3.34){
+  
+  else if(voltage >= 1.66 and voltage <= 3.34){
     Serial.println("Cow");
-    while(voltage >= 1.66 and voltage <= 3.34){
-        updateVoltage();
+    
+    while(voltage >= 0.5){
+        voltage = analogRead(A0) * (5.00 / 1023);
         delay(200);
     }
   }
-  else if(voltage <= 5){
+  
+  else if(voltage >= 3.35 and voltage <= 5){
     Serial.println("Elephant");
-    while(voltage >= 3.35 and voltage <= 5){
-      updateVoltage();
+    
+    while(voltage >= 0.5){
+      voltage = analogRead(A0) * (5.00 / 1023);
       delay(200);
     }
   }
