cdfc4687f56d1d6fdfac3daaadde1c9c49ed55a3
gabrielsclima <gabrielsclima.2004@gmail.com>
2022-09-13 17:27:18 -0300

    Fixed some identations and logic problems
    -By using the Arduino and the physical potentiometer, there was a problem where Giraffe would be returned infinitely
    -Fixed blocks identations in the conditions/loops
    -Added comments explaining the calibration logic

--- voltage-value.ino
+++ voltage-value.ino
@@ -8,52 +8,45 @@ void setup(){
 }
 
 void loop(){
-  Serial.println("Etapa 0");
   t1 = 0;
+  Serial.println("Waiting and checking...");
   
-  while(voltage <= 0.54 or t1 < 24){
-      voltage = analogRead(A0) * (5.00 / 1023);
+  //calib-logic.py, but converted in c++ logic and with the potentiometer input;
+  while(voltage <= 0.54 or t1 < 15){ //t1 * 100 is the time in milleseconds to confirm the calibration;
+    voltage = analogRead(A0) * (5.00 / 1023);
+    
+    if (voltage >= 0.54 and voltage >= (lastVoltage - (lastVoltage / 10)) and voltage <= (lastVoltage + lastVoltage / 10)){
+      t1 += 1;
+    }
+    else{
+      t1 = 0;
+    }
 
-      if (voltage <= 0.54){
-        t1 = 0;
-        }
-      
-      else if (voltage >= (lastVoltage - (lastVoltage / 10)) and voltage <= (lastVoltage + lastVoltage / 10)){
-        t1 += 1;
-        }
-      
-      else{
-        t1 = 0;
-        }
-      delay(100);
-      lastVoltage = voltage;
+    delay(100);
+    lastVoltage = voltage;
   }
-   Serial.println("Done!");
+
+  Serial.println("Done!");
+  Serial.print("Animal: ");
   
   if (voltage >= 0.55 and voltage <= 1.65){
     Serial.println("Giraffe");
-    
     while(voltage >= 0.5){
       voltage = analogRead(A0) * (5.00 / 1023);
-      delay(200);
     }
   }
   
   else if(voltage >= 1.66 and voltage <= 3.34){
     Serial.println("Cow");
-    
     while(voltage >= 0.5){
         voltage = analogRead(A0) * (5.00 / 1023);
-        delay(200);
     }
   }
   
   else if(voltage >= 3.35 and voltage <= 5){
     Serial.println("Elephant");
-    
     while(voltage >= 0.5){
       voltage = analogRead(A0) * (5.00 / 1023);
-      delay(200);
     }
   }
 }
